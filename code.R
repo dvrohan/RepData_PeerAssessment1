2b96abbf130ae5a654a20ad5ad7b234900ce8657
dvrohan <dvrohan04@gmail.com>
2020-05-25 17:21:04 +0530

    Found out Average Daily Activity Pattern

--- code.R
+++ code.R
@@ -19,3 +19,15 @@ hist(Grouped_data$total_steps, xlab = "Steps", main = "Total Daily Steps", col =
 
 print(paste("Mean number of steps taken daily", mean(Grouped_data$total_steps)))
 print(paste("Median number of steps taken daily", median(Grouped_data$total_steps)))
+
+### Average Daily Activity Pattern
+library(ggplot2)
+Mean_Data_By_Interval <- aggregate(activity_data$steps, by=list(activity_data$interval), mean)
+names(Mean_Data_By_Interval) <- c("interval", "steps")
+
+ggplot(Mean_Data_By_Interval, aes(x = interval, y=steps)) +
+  labs(title = "Sum of Steps by Interval", x = "interval", y = "steps")+
+  geom_line(color="red") 
+
+maxInterval <- Mean_Data_By_Interval[which.max(Mean_Data_By_Interval$steps),]
+maxInterval
